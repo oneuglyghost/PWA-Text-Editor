89032e6923efe020d19689817dd37b6a1e5d3d16
Bernardo Alonso <bernardo4430@gmail.com>
2024-04-01 21:06:59 -0500

    added logic for put and get to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB( "jate", 1);
+  const tx =db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  await store.put(content);
+  console.log("Content added to database:", content);
+  await tx.done;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const allContent = await store.getAll();
+  console.log("All content from database:", allContent);
+  await tx.done;
+  return allContent;
+};
 
-initdb();
+
+(async () => {
+  await initdb();
+
+  await putDb({name: "John Doe", age: 30});
+  await putDb({name: "Jane Smith", age: 25});
+  const allContent = await getDb();
+  console.log(allContent);
+})();
+
